d488155f4c95b7413511fd3148896b4d9b523e08
VargasA <43021559+RenatoVargasAlfaro@users.noreply.github.com>
2020-08-16 12:05:47 -0500

    bd en la nube

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    URL: process.env.DB || 'mongodb://localhost:27017',
-    //URL: process.env.DB || 'mongodb+srv://renato:renato12345678@cluster0.gxeej.mongodb.net/Culturaeeee?retryWrites=true&w=majority',
+    //URL: process.env.DB || 'mongodb://localhost:27017',
+    URL: process.env.DB || 'mongodb+srv://renato:renato12345678@cluster0.gxeej.mongodb.net/Culturaeeee?retryWrites=true&w=majority',
     
     /**TOKEN***********************************************/
     /** estaesmillavesecreta*/
