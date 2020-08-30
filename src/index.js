f3ed68b9750eab1b21969ff8529e346cd4faf404
VargasA <43021559+RenatoVargasAlfaro@users.noreply.github.com>
2020-08-29 23:52:28 -0500

    normal

--- src/index.js
+++ src/index.js
@@ -29,17 +29,7 @@ app.set('port', process.env.PORT || 8000);
 //app.set('view engine', 'ejs');
 
 //middlewares
-//app.use(cors());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false})); //permite entender los datos que se envia desde un formulario html
