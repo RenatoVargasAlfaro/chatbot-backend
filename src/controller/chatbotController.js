468454ae5727490542352b2e48d72a0f49a9fd41
VargasA <43021559+RenatoVargasAlfaro@users.noreply.github.com>
2020-09-19 21:12:56 -0500

    crear chatbot

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -4,6 +4,9 @@ const jwt = require("jsonwebtoken");
 const { response } = require('express');
 
 const connection = require('../connection/db-NuevasPreguntas');
+const connection2 = require('../connection/db-Chatbot');
+
+const { ObjectID } = require('mongodb');
 
 async function getChatbot(req, res) {
 
@@ -36,8 +39,13 @@ async function getChatbot(req, res) {
 				console.log('DialogFlow.sendTextMessageToDialogFlow: Detected intent');
 				console.log(respuesta[0].queryResult.fulfillmentText)
 
-				var arreglo = ["dolor en la frente", "dolor de frente", "dolor", "frente", "enrojecimiento de ojo",
-					"ojo rojo", "rojo", "vision borrosa", "borrosa", "vision"]
+				var arreglo = ["dolor en la frente", "dolor de frente", "dolor", "frente", "frente dolor", "fastidio en la frente", "fastidia la frente",
+								"molestia en la frente", "molestia la frente", "duele la frente", "duele mi frente", "dolor en mi frente", "mi frente", "duele",
+								"incomodidad en la frente", "molestia en la frente", "molestia en frente", "molestia frente", "ojo enrojecido", "enrojecimiento de ojo",
+								"ojo rojo", "rojo", "vision borrosa", "borrosa vision", "vision algo borrosa", "borrosa", "vision", "molestia ojo", "borrosa mi vision", 
+								"no veo bien", "veo nublado", "no veo correctamente", "vision nublada", "molestia en el ojo", "dolor en el ojo", "dolor de ojo",
+								"incomodidad en mi frente", "incomodidad en la frente", "frente con incomodidad", "incomodidad en el ojo", "ojo incomodo",
+								"incomodidad en mi ojo", "malestar en mi frente", "malestar en la frente", "malestar en el ojo", "malestar en mi ojo"]
 				
 				var band = false;
 
@@ -279,12 +287,300 @@ async function updateIntent(intent) {
 
 
 
+async function createIntent(req,res) {
+	try {
+
+		var en = req.body.enfermedad;
+		var nombre = req.body.nombre;
+		var desc = req.body.desc;
+		var pgta = req.body.pgta;
+		var rpta = req.body.rpta;
+		const displayName = 'Welcome Intent -' + en;
+		const displayName2 = 'Discomfort Intent - ' + en;
+		const displayName3 = 'Discomfort Fallback Intent - ' + en;
+
+		const chatbot = new clasedialog.DialogFlow(clasedialog.projectId)
+		const agentPath = chatbot.intentsClient.projectAgentPath(chatbot.projectId);
 
 
+		/*-------------------------------INTENCION BIENVENIDA-------------------------------------------*/
+
+		const trainingPhrasesParts1 = ['Hola ' + en,'Hi ' + en];
+		const messageTexts1 = ['Hola soy ' + nombre, ' ¿En que puedo ayudarte?'];
+
+		const trainingPhrases1 = [];
+
+		trainingPhrasesParts1.forEach(trainingPhrasesPart => {
+			const part = {
+				text: trainingPhrasesPart,
+			};
+
+			// Here we create a new training phrase for each provided part.
+			const trainingPhrase1 = {
+				type: 'EXAMPLE',
+				parts: [part],
+			};
+
+			trainingPhrases1.push(trainingPhrase1);
+		});
+
+		const messageText1 = {
+			text: messageTexts1,
+		};
+
+		const message1 = {
+			text: messageText1,
+		};
+
+		const outputContexts1 = [
+			{
+			  name : 'projects/oftalbot-rsd9/agent/sessions/-/contexts/Saludo' + en,
+			  lifespanCount : 1,
+			}
+		]
+
+		const intent = {
+			displayName: displayName,
+			trainingPhrases: trainingPhrases1,
+			messages: [message1],
+			outputContexts : outputContexts1
+		};
+		const createIntentRequest = {
+			parent: agentPath,
+			intent: intent,
+		};
+
+
+		/*-------------------------INTENCION DEL SINTOMA----------------------------*/
+		const trainingPhrasesParts2 = [pgta];
+		const messageTexts2 = [rpta];
+
+		const trainingPhrases2 = [];
+
+		trainingPhrasesParts2.forEach(trainingPhrasesPart => {
+			const part = {
+				text: trainingPhrasesPart,
+			};
+
+			// Here we create a new training phrase for each provided part.
+			const trainingPhrase2 = {
+				type: 'EXAMPLE',
+				parts: [part],
+			};
+
+			trainingPhrases2.push(trainingPhrase2);
+		});
+
+		const messageText2 = {
+			text: messageTexts2,
+		};
+
+		const message2 = {
+			text: messageText2,
+		};
+
+
+		const inputcontext2 = ['projects/oftalbot-rsd9/agent/sessions/-/contexts/Saludo' + en];
+
+		const outputContexts2 = [
+			{
+			  name : 'projects/oftalbot-rsd9/agent/sessions/-/contexts/Receta',
+			  lifespanCount : 1,
+			}
+		]
+
+		const intent2 = {
+			displayName: displayName2,
+			trainingPhrases: trainingPhrases2,
+			messages: [message2],
+			inputContextNames : inputcontext2,
+			outputContexts : outputContexts2
+		};
+		const createIntentRequest2 = {
+			parent: agentPath,
+			intent: intent2,
+		};
+
+
+		/*------------------------------------------INTENCION BUCLE---------------*/
+
+		const messageTexts3 = ['No comprendo lo consultado, ¿Podrías repetirlo?'];
+
+		const messageText3 = {
+			text: messageTexts3,
+		};
+		const message3 = {
+			text: messageText3,
+		};
+		const inputcontext3 = ['projects/oftalbot-rsd9/agent/sessions/-/contexts/Saludo' + en];
+
+		const outputContexts3 = [
+			{
+			  name : 'projects/oftalbot-rsd9/agent/sessions/-/contexts/Saludo' + en,
+			  lifespanCount : 1,
+			}
+		]
+
+		const intent3 = {
+			displayName: displayName3,
+			isFallback: true,
+			messages: [message3],
+			inputContextNames : inputcontext3,
+			outputContexts : outputContexts3
+		};
+		const createIntentRequest3 = {
+			parent: agentPath,
+			intent: intent3,
+		};
+
+
+		let listaintenciones = []
+		await listIntents().then(v => {
+			console.log(v)
+			v[0].forEach((e,i) => {
+				listaintenciones.push(e.displayName)
+			})
+		});
+		console.log("-------------------------------")
+		console.log(listaintenciones)
+
+		band = listaintenciones.find(element => element == displayName2 || element == displayName3 || element == displayName);
+
+		if(!band){
+			// Create the intent
+			const [response] = await chatbot.intentsClient.createIntent(createIntentRequest);
+			const [response2] = await chatbot.intentsClient.createIntent(createIntentRequest2);
+			const [response3] = await chatbot.intentsClient.createIntent(createIntentRequest3);
+
+			console.log("----------------------------------------------")
+			console.log(response);
+			console.log("----------------------------------------------")
+			console.log(response2);
+			console.log("----------------------------------------------")
+			console.log(response3);
+
+			const db = await connection2(); // obtenemos la conexión
+			const newBot = {
+				nombre: nombre,
+				enfermedad: en,
+				desc: desc,
+				intencion1: displayName,
+				intencion2: displayName2,
+				intencion: displayName3,
+				id1: response.name,
+				id2: response2.name,
+				id3: response3.name,
+			}
+			await db.collection('chatbot').insertOne(newBot, (err, result) => {
+				if (err) throw err;
+				console.log("chatbot agregado");
+				//res.json("Agregado");
+				res.json(newBot);
+			});
+		} else {
+			return res.status(400).json({
+                message: 'NO FUNCIONO'
+            });
+		}
+
+	} catch (err) {
+		return new Promise((resolve, reject) => {
+			reject(err)
+			console.log(err)
+		})
+	}
+}
+
+
+async function listIntents() {
+  // Construct request
+
+  		const chatbot = new clasedialog.DialogFlow(clasedialog.projectId)
+		const agentPath = chatbot.intentsClient.projectAgentPath(chatbot.projectId);
+
+  		console.log(agentPath);
+
+		const request = {
+			parent: agentPath,
+		};
+		let lista = await chatbot.intentsClient.listIntents(request);
+		const variable =  lista;
+		return variable;
+}
+
+
+async function deleteIntents(req, res) {
+	
+	const chatbot = new clasedialog.DialogFlow(clasedialog.projectId)
+	const agentPath = chatbot.intentsClient.projectAgentPath(chatbot.projectId);
+	//console.log(agentPath);
+
+	const db = await connection2(); // obtenemos la conexión
+	const dato = req.params.id; 
+
+	
+	let bot = 1;
+	await db.collection('chatbot').find({_id: ObjectID(dato)}).toArray().then(e => bot = e);
+
+	console.log(bot[0].nombre);
+	var id1 = bot[0].id1.split('/')[4]
+	var id2 = bot[0].id2.split('/')[4]
+	var id3 = bot[0].id3.split('/')[4]
+
+	const intentPath = chatbot.intentsClient.intentPath(chatbot.projectId, id1);
+	const request = {name: intentPath};
+	// Send the request for deleting the intent.
+	const result = await chatbot.intentsClient.deleteIntent(request);
+	console.log(`Intent ${intentPath} deleted`);
+
+
+	const intentPath2 = chatbot.intentsClient.intentPath(chatbot.projectId, id2);
+	const request2 = {name: intentPath2};
+	// Send the request for deleting the intent.
+	const result2 = await chatbot.intentsClient.deleteIntent(request2);
+	console.log(`Intent ${intentPath2} deleted`);
+
+
+	const intentPath3 = chatbot.intentsClient.intentPath(chatbot.projectId, id3);
+	const request3 = {name: intentPath3};
+	// Send the request for deleting the intent.
+	const result3 = await chatbot.intentsClient.deleteIntent(request3);
+	console.log(`Intent ${intentPath3} deleted`);
+
+
+	await db.collection('chatbot').deleteOne({
+		_id: ObjectID(dato)
+	}, (err, obj) => {
+		if (err) throw err;
+		console.log("Dato borrado");
+		res.json("Borrado");
+	});
+
+	//return result;	
+
+}
+
+
+async function getIntents(req, res) {
+	const db = await connection2(); // obtenemos la conexión
+	//var docs = await db.collection('cultura').find().toArray();
+	//res.json(docs);
+	await db.collection('chatbot').find().toArray((err, result) => {
+		if (err) throw err;
+		console.log("datos obtenidos");
+		res.json(result);
+	});
+}
+
+
+  
 
 module.exports = {
 	getChatbot,
 	//addPhraTrainingPhrases,
 	//addPhraTrainingResponse,
-	testUpdateTraining
+	testUpdateTraining,
+	createIntent,
+	deleteIntents,
+	getIntents
 }
--- src/routes/chatbotRouter.js
+++ src/routes/chatbotRouter.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const userCtrl = require('../controller/chatbotController');
 
 router.post('/', userCtrl.getChatbot);
+router.post('/add', userCtrl.createIntent)
+router.delete('/delete/:id', userCtrl.deleteIntents)
+router.get('/getchatbot', userCtrl.getIntents)
 
 module.exports = router;
